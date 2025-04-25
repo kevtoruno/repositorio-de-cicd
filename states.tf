ad7a9a0aea9c9c3b250d35c4d9cb3b4edc6cfab2
ktoruno-paynau <ktoruno@paynau.com>
2025-04-24 21:00:07 -0600

    corrigiendo region

--- states.tf
+++ states.tf
@@ -3,10 +3,10 @@ terraform{
         bucket = "platzi-terraform-state-keiktsu"
         encrypt = true
         key = "terraform1.tfstate"
-        region = "us-east-1"
+        region = "us-east-2"
     }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-east-2"
 }
